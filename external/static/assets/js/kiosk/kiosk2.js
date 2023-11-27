d81adc84e23d145cc9ea986ae2c8da2306d3a755
김가영 <63998250+GaYoung0420@users.noreply.github.com>
2023-11-27 23:38:54 +0900

    2023-11-27

--- external/static/assets/js/kiosk/kiosk2.js
+++ external/static/assets/js/kiosk/kiosk2.js
@@ -1,22 +1,47 @@
 let json = $('.MenuContainer').css('border', 'solid 1px red');
 
 let menuData = JSON.parse(JSON.stringify(MenuFile));
-console.log(menuData);
+let select_category = 'coffee';
 
-var num = '2';
-$('#menu' + num)
-  .find('img')
-  .attr('src', '../img/menu/digital_cafe_latte.png');
+// var num = '2';
+// $('#menu' + num)
+//   .find('img')
+//   .attr('src', '../img/menu/digital_cafe_latte.png');
+
+// console.log(category_num);
+
+
+
+
 
 const change_menu = (category) => {
+  var category_num = Math.ceil(menuData[category][0].length / 6);
+  set_paging_button(category_num);
   menuData[category][0].forEach((element, index) => {
     console.log(element);
   });
 };
 
+const set_paging_button = (category_num)=>{
+
+  for(let i=0; i<category_num; i++){
+    if(i=0)
+  }
+  
+}
+
+change_menu(select_category);
+
+
+/*
+
+page에 따라서 메뉴 흩뿌리기
+
 1 2 3 4 5 6
 7 8 9 10 11 12
+
+
 index+1
 
+*/
 
-change_menu('coffee');
